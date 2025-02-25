f4c53298da07697a27ea82c7779dc5f3f31cfc31
Timurbek <timurbeksaburovv.@gmail.com>
2025-02-25 13:57:34 +0500

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Zakaz from "./Components/Zakaza"
+import Zakaz from "./Components/Zakaza";
 import HomePage from "./Pages/HomePage";
 import ProductDetail from "./Pages/ProductDetail";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import api from "./api";
-import NewsProvider, { useStateValue } from "./context/newsContext";
 import News from "./Pages/NewsPage";
 import Newss from "./Pages/Korzinka";
+import Layout from "./Components/Layout";
+import api from "./api";
+import NewsProvider from "./context/newsContext";
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -21,22 +20,21 @@ const App = () => {
   return (
     <NewsProvider>
       <Router>
-        <Header setProducts={setProducts} cart={cart} setCart={setCart} />
         <Routes>
           <Route
             path="/"
-            element={
-              <HomePage products={products} cart={cart} setCart={setCart} />
-            }
-          />
-          <Route path="/news" element={<News />} />
-          <Route path="/zakaz" element={<Zakaz/>} />
-          <Route path="/product/:productId" element={<ProductDetail />} />
+            element={<Layout setProducts={setProducts} cart={cart} setCart={setCart} />}
+          >
+            <Route index element={<HomePage products={products} cart={cart} setCart={setCart} />} />
+            <Route path="news" element={<News />} />
+            <Route path="zakaz" element={<Zakaz />} />
+            <Route path="product/:productId" element={<ProductDetail />} />
+          </Route>
         </Routes>
-        <Footer />
       </Router>
     </NewsProvider>
   );
 };
 
 export default App;
+
